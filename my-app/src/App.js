abb88b72ce8a376373ffd231ba5a6f519773cab7
Kebira <kebiraidehmad@gmail.com>
2023-08-19 17:03:56 +0100

    created a memory game

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+
+import Cards from './Components/Cards';
+
+const App = () => {
+  
+    return (
+      <div className='App'>
+        <h1>Memory Game</h1>
+        <Cards />
+
+      </div>
+    );
+  };
+
+ 
+  
+
+export default App;
